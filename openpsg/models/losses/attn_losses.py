f7aef6edc68b6bbb2ddc2ac3af5a986962e11457
fuqianya <fuqian_918@163.com>
2022-12-13 22:03:05 +0800

    :bug: Fix bug in omit attn_losses.py

---
+++ openpsg/models/losses/attn_losses.py
@@ -0,0 +1,21 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from mmdet.models.builder import LOSSES
+
+
+@LOSSES.register_module()
+class AttnMarginLoss(nn.Module):
+    def __init__(self):
+        super(AttnMarginLoss, self).__init__()
+        self.margin_ranking_loss = nn.MarginRankingLoss()
+
+    def forward(self, inputs1, inputs2, targets):
+        num_objs = inputs1.shape[0]
+
+        loss = 0.
+        for input1, input2, target in zip(inputs1, inputs2, targets):
+            loss += self.margin_ranking_loss(input1, input2, target)
+        return loss
